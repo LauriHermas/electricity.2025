b1054e2705b27a4dee82f3d2b42e088ffaeabdba
LauriHermas <lauri.hermas@gmail.com>
2025-10-20 19:58:47 +0300

    Refactor data processing for visualization
    Refactor code to calculate averages and totals for visualization.

--- finalproject.py
+++ finalproject.py
@@ -24,16 +24,6 @@ df['bill']=df['Price']*df['kWh']
 
 Interval = 'D' or 'W' or 'M'  #D=Daily W=Weekly M=Monthly
 
-df_filtered=df.groupby(pd.Grouper(key = 'datetime', freq = Interval)).agg(
-    avg_price=('Price', 'mean'),
-    avg_temp=('Temperature', 'mean'),
-    total_consumption=('kWh', 'sum'),
-    total_bill=('bill', 'sum')
-).reset_index()
-
-#convert bill to euros
-df_filtered['total_bill'] = df_filtered['total_bill']/100
-
 #visualization part
 st.title('Energy Consumption and price')
 
@@ -43,14 +33,15 @@ date_after_start = df['datetime'] >= pd.to_datetime(start_date)
 #Selector for end date
 end_date = st.date_input("Select an end date", value=df['datetime'].max().date())
 date_before_end = df['datetime'] <= pd.to_datetime(end_date)
+
 #filter the data frame according to the selected dates
-df_filtered =  df[date_after_start & date_before_end]
+df =  df[date_after_start & date_before_end]
 
 st.write("Showing range: ", start_date, " - ", end_date)
-st.write("Total consumption over the period: ", df_filtered['kWh'].sum().round(1), "kWh")
-st.write("Total bill over the period: ", (df_filtered['bill'].sum()/100).round(1), "€")
-st.write("Average hourly price: ", df_filtered['Price'].mean().round(2), "cents")
-st.write("Average paid price: ", (df_filtered['bill'].sum()/df_filtered['kWh'].sum()).round(2), "cents")
+st.write("Total consumption over the period: ", df['kWh'].sum().round(1), "kWh")
+st.write("Total bill over the period: ", (df['bill'].sum()/100).round(1), "€")
+st.write("Average hourly price: ", df['Price'].mean().round(2), "cents")
+st.write("Average paid price: ", (df['bill'].sum()/df['kWh'].sum()).round(2), "cents")
 
 #selector for interval
 interval_group = st.multiselect(label='Select a period',options=['Daily','Weekly','Monthly'],default='Monthly',max_selections=1)
@@ -63,13 +54,18 @@ if(len(interval_group)>=1):
     elif(interval_group[0]=='Monthly'):
         interval = 'M'
 
+df_avgP=df.groupby(pd.Grouper(key = 'datetime', freq = Interval))[['Price']].mean().reset_index()
+df_avgT=df.groupby(pd.Grouper(key = 'datetime', freq = Interval))[['Temperature']].mean().reset_index()
+df_totalC=df.groupby(pd.Grouper(key = 'datetime', freq = Interval))[['kWh']].sum().reset_index()
+df_totalB=df.groupby(pd.Grouper(key = 'datetime', freq = Interval))[['bill']].sum().reset_index()
+
+#convert bill to euros
+df_totalB['bill'] = df_totalB['bill']/100
 
 #draw the line charts
-st.line_chart(df_filtered, x = 'datetime', y = 'Price', x_label ='Time', y_label='Average Price €,cents')
-st.line_chart(df_filtered, x = 'datetime', y = 'Temperature', x_label ='Time', y_label='Average Temperature °C')
-st.line_chart(df_filtered, x = 'datetime', y = 'bill', x_label ='Time', y_label='Total Bill €')
-st.line_chart(df_filtered, x = 'datetime', y = 'kWh', x_label ='Time', y_label='Total Consumption kWh')
-
-
+st.line_chart(df_avgP, x = 'datetime', y = 'Price', x_label ='Time', y_label='Average Price €,cents')
+st.line_chart(df_avgT, x = 'datetime', y = 'Temperature', x_label ='Time', y_label='Average Temperature °C')
+st.line_chart(df_totalB, x = 'datetime', y = 'bill', x_label ='Time', y_label='Total Bill €')
+st.line_chart(df_totalC, x = 'datetime', y = 'kWh', x_label ='Time', y_label='Total Consumption kWh')
 
 
